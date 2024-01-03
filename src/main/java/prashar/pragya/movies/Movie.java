0e7b2661e29e6068aa396e55d64057e8b9cfd051
Pragya <pragyaprashar490@gmail.com>
2024-01-03 15:20:02 -0500

    first commit

---
+++ src/main/java/prashar/pragya/movies/Movie.java
@@ -0,0 +1,31 @@
+package prashar.pragya.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Document(collection = "movies")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Movie {
+  @Id
+  private ObjectId id;
+  private String imdbId;
+  private String title;
+  private String releaseDate;
+  private String trailerLink;
+  private String poster;
+  private List<String> genres;
+  private List<String> backdrops;
+  @DocumentReference  //store the ids of the reviews and reviews will be in another relation
+  private List<Review> reviewIds;
+
+}
---
+++ src/main/java/prashar/pragya/movies/ReviewController.java
@@ -0,0 +1,26 @@
+package prashar.pragya.movies;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("/api/v1/reviews")
+public class ReviewController {
+  @Autowired
+  private ReviewService reviewService;
+  @PostMapping()
+  public ResponseEntity<Review> createReview(@RequestBody Map<String,String> payload){
+    return new ResponseEntity<Review>(reviewService.createReview(payload.get("reviewBody"),payload.get("imdbId")), HttpStatus.CREATED);
+  }
+
+
+}
